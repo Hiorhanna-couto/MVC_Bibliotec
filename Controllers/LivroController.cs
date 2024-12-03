4341c810ffd651af44d1456455ebddc76daefddf
Hiorhanna-couto <hiorhannacouto2007@gmail.com>
2024-12-03 16:50:16 -0300

    Cadastrar os Livro

--- Controllers/LivroController.cs
+++ Controllers/LivroController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Bibliotec.Contexts;
 using Bibliotec.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
 using Microsoft.Extensions.Logging;
 
 namespace Bibliotec_mvc.Controllers
@@ -53,12 +54,15 @@ namespace Bibliotec_mvc.Controllers
             // retorna a View de Cadastro:
             return View();
         }
+
+
         //metodo para cadastrar u livro:
         [Route("Cadastrar")]
         public IActionResult Cadastrar(IFormCollection form)
         {
-
+            //primeira parte: Cadastro livro na tabela de livro
             Livro novoLivro = new Livro();
+
             // o que o usuario escrever no formulario sera atribuido ao novoLivro
             //Nome
             novoLivro.Nome = form["Nome"].ToString();
@@ -70,15 +74,76 @@ namespace Bibliotec_mvc.Controllers
             novoLivro.Escritor = form["Escritor"].ToString();
             //Idioma
             novoLivro.Idioma = form["Idioma"].ToString();
-               
-           // img  
+
+            // trabalha com  imagen
+            if (form.Files.Count > 0)
+            {
+                //Primeiro Passo :
+                // Armazernaremos o arquivo enviado pelo usuario
+                var arquivo = form.Files[0];
+                //Segundo Passo:
+                //Criar variavel do caminho da minha pasta para calocar as fotos dos livros  
+                var pasta = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/imagens/Livros");
+
+                //Valiadaremos se a pasta que sera armazenadaas imagens , existe .caso nao exista ,criaremos uma nova pasta 
+                if (Directory.Exists(pasta))
+                {
+
+                    //criar a pasta :
+                    Directory.CreateDirectory(pasta);
+
+                }
+                //terceiro passo:
+                //criar  a variavel para armazenar o caminho em que meu arquivo estara ,alem do nome dele 
+                var caminhos = Path.Combine(pasta, arquivo.FileName);
+                using (var stream = new FileStream(caminhos, FileMode.Create))
+                {
+                    //Copiou o arquivos para o meu diretoria 
+                    arquivo.CopyTo(stream);
+
+                }
+
+                novoLivro.Imagem = arquivo.FileName;
+            } else{
+
+
+                novoLivro.Imagem = "padrao.png";
+             }
+                  
+
+
+
             context.Livro.Add(novoLivro);
             context.SaveChanges();
 
-           
+
+            //SEGUNDA  PARTE : E adicinar dentro de LivroCategoria a categoria que pertence ao  novolivro
+            //lista as categoria
+            List<LivroCategoria> listalivroCategorias = new List<LivroCategoria>();
+            //Array que possui as categorias selecionadas pelo usuario
+            string[] categoriasSelecionadas = form["Categoria"].ToString().Split(',');
+            // açao,terro, suspense
+
+            foreach (string categoria in categoriasSelecionadas)
+            {
+                // categoria = 3 ou 5 ou 7
+                //string categoria possui a informacao do id 
+                LivroCategoria livroCategoria = new LivroCategoria();
+
+                livroCategoria.CategoriaID = int.Parse(categoria);
+                livroCategoria.LivroID = novoLivro.LivroID;
+                // Adicionamos o obj livroCategoria dentro  da lista   listalivroCategorias
+                listalivroCategorias.Add(livroCategoria);
 
 
-            return View();
+            }
+            //peguei a colecao da listalivrocategorias e co
+            context.LivroCategoria.AddRange(listalivroCategorias);
+
+            context.SaveChanges();
+
+
+            return LocalRedirect("/Livro/Cadastrar");
         }
 
 
